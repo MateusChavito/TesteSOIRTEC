04d9e9f2586b4f941272ef4a2f828630ba248b34
MateusChavito <mateus.chavito@hotmail.com>
2025-02-07 14:17:12 -0300

    Subindo projeto

---
+++ Desktop/Teste Django/django-junior-master/polls/models.py
@@ -0,0 +1,33 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def total_votes(self):
+        
+        total = 0
+        
+        for choice in self.choice_set.all():
+            total += choice.votes  
+        return total
+
+    def has_votes(self):
+        return self.choice_set.filter(votes__gt=0).exists()
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
